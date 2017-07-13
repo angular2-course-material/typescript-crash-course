7f0c7954a1c98b7ab35a4c19d450d2fac39dc6c0
Daniela Meneses <dvargas@oildex.com>
2017-07-12 21:36:12 -0400

    Separated into files

--- first-steps/index.ts
+++ first-steps/index.ts
@@ -1,23 +1,8 @@
+import {Person} from './person';
+import {Student} from './student';
 
 function helloWorld(person: Person) {
     return `Hello world, ${person.firstname} ${person.lastname}`;
-}
-
-interface Person {
-    firstname: string;
-    lastname: string;
-}
-
-class Student {
-    firstname: string;
-    middlename: string;
-    lastname: string;
-
-    constructor(firstname: string, middlename: string, lastname: string) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.middlename = middlename;
-    }
 }
 
 let dann = new Student('dann', 'ale', 'meneses');
---
+++ first-steps/student.ts
@@ -0,0 +1,11 @@
+export class Student {
+    firstname: string;
+    middlename: string;
+    lastname: string;
+
+    constructor(firstname: string, middlename: string, lastname: string) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.middlename = middlename;
+    }
+}
