f5c3054e373cf94d5e111e36360be7ebd61a44f6
Daniela Meneses <dvargas@oildex.com>
2017-07-12 21:16:39 -0400

    Added interfaces and classes

--- first-steps/index.ts
+++ first-steps/index.ts
@@ -1,6 +1,25 @@
 
-function helloWorld(name) {
-    return `Hello world, ${name}`;
+function helloWorld(person: Person) {
+    return `Hello world, ${person.firstname} ${person.lastname}`;
 }
 
-document.body.innerHTML = helloWorld('dann');
+interface Person {
+    firstname: string;
+    lastname: string;
+}
+
+class Student {
+    firstname: string;
+    middlename: string;
+    lastname: string;
+
+    constructor(firstname: string, middlename: string, lastname: string) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.middlename = middlename;
+    }
+}
+
+let dann = new Student('dann', 'ale', 'meneses');
+
+document.body.innerHTML = helloWorld(dann);
